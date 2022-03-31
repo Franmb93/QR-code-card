0647900a1b2743b667ec1b21099202ab2914b7ad
franmb93 <fcomunozbetanzos@gmail.com>
2022-03-31 09:12:15 +0200

    Deploy QR to GitHub Pages

---
+++ precache-manifest.16215c06a58dcb7b0fb30fff73a77c82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e079e4a96635c94f0d51",
+    "url": "/QR-code-card/static/css/main.87c713f6.chunk.css"
+  },
+  {
+    "revision": "e079e4a96635c94f0d51",
+    "url": "/QR-code-card/static/js/main.e079e4a9.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/QR-code-card/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "b75323fd636df612d81b",
+    "url": "/QR-code-card/static/js/2.b75323fd.chunk.js"
+  },
+  {
+    "revision": "de19ae04d57a3ea3a1c9",
+    "url": "/QR-code-card/static/js/runtime~main.de19ae04.js"
+  },
+  {
+    "revision": "f62580a1fbf474b9e338ed83bf21c010",
+    "url": "/QR-code-card/index.html"
+  }
+];
